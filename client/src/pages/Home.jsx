8f235c3bef97a90147d297e69bfe2c496d7840d3
Nirmal kandel <nirmalkandel001@gmail.com>
2026-07-19 15:18:45 +0530

    Fetch todos in React using Axios

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import API from "../api/api";
+
 function Home() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await API.get("/todos");
+
+      console.log(response.data);
+
+      setTodos(response.data.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
-      Home Page
+      <h1>Todo App</h1>
+
+      {todos.map((todo) => (
+        <p key={todo._id}>{todo.title}</p>
+      ))}
     </div>
   );
 }
